0ea63d22715c37e766f38ea7525176a2e6511787
Nisarg P <np17531@gmail.com>
2024-02-15 02:56:20 +0530

    [=]added search functionality

--- lib/ui/home/sub_category_page.dart
+++ lib/ui/home/sub_category_page.dart
@@ -1,3 +1,4 @@
+import 'package:another_flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
@@ -5,9 +6,11 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:get/get.dart';
 import 'package:wave_app/controller/all_category_controller/all_category_controller.dart';
+import 'package:wave_app/controller/internet_controller/internet_controller.dart';
 import 'package:wave_app/generated/assets.dart';
 import 'package:wave_app/theme/custom_text_style.dart';
 import 'package:wave_app/widgets/custom_image_view.dart';
+import 'package:wave_app/widgets/custom_text_field.dart';
 
 class SubCategoryPage extends StatefulWidget {
   const SubCategoryPage({super.key});
@@ -17,6 +20,8 @@ class SubCategoryPage extends StatefulWidget {
 }
 
 class _SubCategoryPageState extends State<SubCategoryPage> {
+  TextEditingController searchController = TextEditingController();
+  ValueNotifier<bool> showSearchBar = ValueNotifier(false);
   List<String> category = [
     "Astrologers",
     "Caterers",
@@ -44,12 +49,26 @@ class _SubCategoryPageState extends State<SubCategoryPage> {
     "Wedding Planners",
     "YOGA - MEDITATION"
   ];
+  var internetController = Get.put(InternetController());
   var categoryController = Get.put(AllCatController());
 
   @override
   void initState() {
     super.initState();
-    categoryController.getSubCategory("Astrologers");
+    if (internetController.message.value == "Internet Connection Gained") {
+      categoryController.getSubCategory("Astrologers");
+    } else {
+      Flushbar(
+        backgroundColor: const Color(0xffA41C8E),
+        flushbarPosition: FlushbarPosition.BOTTOM,
+        messageText: Text(
+          internetController.message.value,
+          style: const TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ).show(context);
+    }
   }
 
   @override
@@ -73,9 +92,14 @@ class _SubCategoryPageState extends State<SubCategoryPage> {
           ),
         ),
         actions: [
-          Image.asset(
-            Assets.imagesSearch,
-            scale: 1.3,
+          GestureDetector(
+            onTap: () {
+              showSearchBar.value = !showSearchBar.value;
+            },
+            child: Image.asset(
+              Assets.imagesSearch,
+              scale: 1.3,
+            ),
           ),
         ],
       ),
@@ -92,6 +116,30 @@ class _SubCategoryPageState extends State<SubCategoryPage> {
   Column pageBodyWidget() {
     return Column(
       children: [
+        10.verticalSpace,
+        ValueListenableBuilder(
+          valueListenable: showSearchBar,
+          builder: (context, value, child) => value
+              ? TextFieldDesignPage(
+                  onChanged: (val) {
+                    categoryController.getSubCategory(val);
+                  },
+                  edgeInsets: const EdgeInsets.symmetric(
+                    vertical: 5,
+                    horizontal: 15,
+                  ).r,
+                  textInputAction: TextInputAction.done,
+                  textInputType: TextInputType.text,
+                  controller: searchController,
+                  labelText: "Search Category",
+                  prefixWidget: const Icon(
+                    Icons.search,
+                    color: Colors.grey,
+                  ),
+                )
+              : const SizedBox.shrink(),
+        ),
+        15.verticalSpace,
         SizedBox(
           height: 25.h,
           child: ListView.builder(
@@ -102,7 +150,21 @@ class _SubCategoryPageState extends State<SubCategoryPage> {
             itemBuilder: (context, index) {
               return GestureDetector(
                 onTap: () {
-                  categoryController.getSubCategory(category[index]);
+                  if (internetController.message.value ==
+                      "Internet Connection Gained") {
+                    categoryController.getSubCategory(category[index]);
+                  } else {
+                    Flushbar(
+                      backgroundColor: const Color(0xffA41C8E),
+                      flushbarPosition: FlushbarPosition.BOTTOM,
+                      messageText: Text(
+                        internetController.message.value,
+                        style: const TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ).show(context);
+                  }
                 },
                 child: Container(
                   margin: const EdgeInsets.only(right: 15).r,
@@ -117,7 +179,7 @@ class _SubCategoryPageState extends State<SubCategoryPage> {
                     style: TextStyle(
                       color: Colors.white,
                       fontSize: 14.spMin,
-                      fontFamily: "Acme",
+                      fontFamily: "Arial",
                     ),
                   ),
                 ),
@@ -126,7 +188,7 @@ class _SubCategoryPageState extends State<SubCategoryPage> {
           ),
         ),
         Container(
-          margin: const EdgeInsets.only(top: 5).r,
+          margin: const EdgeInsets.only(top: 8).r,
           padding: const EdgeInsets.symmetric(vertical: 15).r,
           decoration: BoxDecoration(
             color: Colors.white,
@@ -153,7 +215,7 @@ class _SubCategoryPageState extends State<SubCategoryPage> {
                 style: TextStyle(
                   color: Colors.black,
                   fontSize: 14.spMin,
-                  fontFamily: "Acme",
+                  fontFamily: "Arial",
                 ),
               ),
               const Spacer(),
@@ -168,7 +230,7 @@ class _SubCategoryPageState extends State<SubCategoryPage> {
                 style: TextStyle(
                   color: Colors.black,
                   fontSize: 14.spMin,
-                  fontFamily: "Acme",
+                  fontFamily: "Arial",
                 ),
               ),
               const Spacer(),
@@ -182,58 +244,65 @@ class _SubCategoryPageState extends State<SubCategoryPage> {
           ),
         ),
         GetBuilder<AllCatController>(
-          builder: (controller) => Expanded(
-            child: SingleChildScrollView(
-              child: AlignedGridView.count(
-                physics: const NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 15, vertical: 5).r,
-                itemCount:
-                    controller.subCategoryResponseModel.value?.data?.length ??
-                        0,
-                crossAxisSpacing: 5,
-                mainAxisSpacing: 20,
-                crossAxisCount: 2,
-                itemBuilder: (context, index) {
-                  final subCategory =
-                      controller.subCategoryResponseModel.value?.data?[index];
-                  return Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 20).r,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        5.verticalSpace,
-                        Center(
-                          child: CustomImageView(
-                            height: 80.h,
-                            imagePath: subCategory?.thumbnail,
-                          ),
+          builder: (controller) =>
+              controller.subCategoryResponseModel.value?.data != null
+                  ? Expanded(
+                      child: SingleChildScrollView(
+                        child: AlignedGridView.count(
+                          physics: const NeverScrollableScrollPhysics(),
+                          shrinkWrap: true,
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 25,
+                            vertical: 5,
+                          ).r,
+                          itemCount: controller.subCategoryResponseModel.value
+                                  ?.data?.length ??
+                              0,
+                          crossAxisSpacing: 5,
+                          mainAxisSpacing: 10,
+                          crossAxisCount: 2,
+                          itemBuilder: (context, index) {
+                            final subCategory = controller
+                                .subCategoryResponseModel.value?.data?[index];
+                            return Container(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 15,
+                                vertical: 3,
+                              ).r,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  5.verticalSpace,
+                                  Center(
+                                    child: CustomImageView(
+                                      height: 80.h,
+                                      imagePath: subCategory?.thumbnail,
+                                    ),
+                                  ),
+                                  15.verticalSpace,
+                                  ratingBarRow(),
+                                  5.verticalSpace,
+                                  Text(
+                                    subCategory?.name ?? "",
+                                    style: CustomTextStyles.bodyMediumGrey13,
+                                  ),
+                                  5.verticalSpace,
+                                  Text(
+                                    "Rs ${subCategory?.price}" ?? "",
+                                    style: CustomTextStyles.bodySmallff222222,
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
                         ),
-                        15.verticalSpace,
-                        ratingBarRow(),
-                        3.verticalSpace,
-                        Text(
-                          subCategory?.subcatg ?? "",
-                          style: CustomTextStyles.bodyMediumGrey13,
-                        ),
-                        3.verticalSpace,
-                        Text(
-                          subCategory?.name ?? "",
-                          style: CustomTextStyles.bodyLargeBlack900_1,
-                        ),
-                        3.verticalSpace,
-                        Text(
-                          "Rs ${subCategory?.price}" ?? "",
-                          style: CustomTextStyles.bodySmallff9b9b9b11,
-                        ),
-                      ],
+                      ),
+                    )
+                  : const Center(
+                      child: Text(
+                        "No Data Found",
+                      ),
                     ),
-                  );
-                },
-              ),
-            ),
-          ),
         )
       ],
     );
